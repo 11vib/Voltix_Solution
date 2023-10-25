e926eacd3f4773ef5bf9bc5fe240fcedb6bed25a
11vib <vibhahb9140@gmail.com>
2023-10-25 16:13:37 +0530

    yarn

---
+++ front/src/components/testimonials.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+interface ITestimonial {
+    _id:string;
+  testimonial_author: string;
+  testimonial_content: string;
+  created_at: Date;
+  created_by: string;
+}
+
+const Testimonials: React.FC = () => {
+  const [testimonials, setTestimonials] = useState<ITestimonial[]>([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/tapi/testimonials")
+      .then((response) => {
+        setTestimonials(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching the testimonials:", error);
+      });
+  }, []);
+
+  return(
+    <div className="container">
+        <h1>Testimonials</h1>
+        <div className="card-deck">
+            {testimonials.map((entry)=>(
+                <div className="card" key={entry._id}>
+                    <div className="card-body">
+                        <h5 className="card-title">{entry.testimonial_author}</h5>
+                        <p className="card-text">{entry.testimonial_content}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    </div>
+  );
+};
+
+export default Testimonials;
---
+++ front/src/pages/TestimonialsPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Testimonials from "../components/testimonials";
+
+const TestimonialsPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Testimonials Page</h1>
+      <Testimonials />
+    </div>
+  );
+};
+
+export default TestimonialsPage;

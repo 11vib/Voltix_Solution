84ca6dd56bde30fdfb1702a5496dec5265525215
11vib <vibhahb9140@gmail.com>
2023-10-26 08:28:42 +0530

    projects and services

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TestimonialsPage from './pages/TestimonialsPage';
 import ProjectsPage from './pages/ProjectsPage';
+import ServicesPage from './pages/ServicesPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/testimonials" element={<TestimonialsPage />} />
         <Route path="/projects" element={<ProjectsPage />} />
+        <Route path='/services' element={<ServicesPage />} />
       </Routes>
     </BrowserRouter>
   );
